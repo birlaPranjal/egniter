1793053d837307bc5918fd6bda9fc72f8fa3de5a
Pranjal Birla <birlapranjal460@gmail.com>
2024-11-16 16:21:59 +0530

    added comming soon image for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import soon from "../../public/big.png";
+import mobile from "../../public/mobile.svg";
 import { FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen">
-      <Image src={soon} alt="Coming Soon" className="h-full w-full object-cover" />
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-16 flex space-x-4">
+    <div className="relative h-[92.5vh] md:h-screen w-screen">
+      <Image src={soon} alt="Coming Soon" className="h-full w-full object-cover sm:block hidden" />
+      <Image src={mobile} alt="Coming Soon" className="h-full w-full object-cover block sm:hidden" />
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-10 md:mb-16 flex space-x-4">
         <a 
           href="https://www.instagram.com/egniterproduction" 
           target="_blank" 
