047714ebb726f563ae6e4628b0af3bcf12fc885c
Pranjal Birla <birlapranjal460@gmail.com>
2025-03-01 12:00:40 +0530

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 'use client'
 import React from 'react';
-import Navbar from '@/app/components/Navbar';
-import Hero from '@/app/components/Hero';
-import Services from '@/app/components/Services';
 import About from '@/app/components/About';
 import Work from '@/app/components/Work';
 import Clients from '@/app/components/Clients'
 import Contact from '@/app/components/Contact'
 import Footer from './components/Footer';
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import("./components/Navbar"), {
+  ssr: false, // Disables server-side rendering
+});
+
+const Hero = dynamic(() => import("./components/Hero"), {
+  ssr: false, // Disables server-side rendering
+});
+
+const Services = dynamic(() => import("./components/Services"), {
+  ssr: false, // Disables server-side rendering
+});
 
 const Page: React.FC = () => {
   return (
